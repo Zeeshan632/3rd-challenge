cda7b8f9d824143fab9af8e396868a69be63ed3c
zeeshanali <zeeshanadil632@gmail.com>
2020-11-28 09:12:56 +0500

    added some code of practicing some codes of javascript array methods and code wars in new branch

--- app.js
+++ app.js
@@ -74,7 +74,108 @@ codedOp6.textContent = '<script src="xxx.js">';
 
 
 
+// ******************************************************
+// Exercise for CodeWars
+// ******************************************************
 
+
+
+const likeFinder = (array) => {
+    array.splice(-1, 0, 'and');
+    let myString = array.toString();
+    return `${myString} liked this`; 
+};
+
+
+
+let numbers = [1, 2, 3, 4, 5];
+let results = numbers.splice(2, 0, 6, 7)
+console.log(numbers)
+
+
+// *_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_
+// This is my way of solving probelem
+
+function likes(names) {
+    if(names.length === 0){
+        return `no one likes this`;
+    }else if(names.length === 1){
+        return `${names.toString()} likes this`;
+    }else if(names.length === 2){
+        return `${names[0]} and ${names[1]} likes this`; 
+    }else if(names.length === 3){
+        names.splice(1, 2, ` ${names[1]} and ${names[2]}`);
+        names.toString();
+        return `${names} likes this`;
+    }else {
+        names.splice(1, names.length-1, ` ${names[1]} and ${names.length-2} others`);
+        names.toString();
+        return `${names} likes this`;
+    }
+};
+
+// *_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*
+//This is the preferred way of solving probelem
+
+function likes2(names) {
+    switch (names.length){
+        case 0: return 'on one likes this'; break;
+        case 1: return names[0] + ' likes this'; break;
+        case 2: return names[0] + ' ' + names[1] + ' like this'; break;
+        case 3: return names[0] + ' , ' + names[1] + ' and ' + names[2] + ' like this'; break;
+        default: return names[0] + ' , ' + names[1] + ' and ' + (names.length - 2) + ' others like this';
+    } 
+}
+
+console.log(likes(['zeeshan', 'arsalan', 'broski', 'brobro']));
+console.log(likes2(['zeeshan', 'arsalan', 'broski', 'brobro']));
+
+
+
+// ******************************************
+// MOST IMPORTANT ARRAY METHODS
+// ******************************************
+
+// Filter method
+const prices = [10, 20, 67, 33, 47, 36, 39];
+
+const affordableQuality = prices.filter((e) => e > 20 && e < 45);
+console.log(affordableQuality);
+
+const teamPlayers = [
+    { name: 'John', height: 6.2 },
+    { name: 'Ghansham', height: 5.6 },
+    { name: 'Brobro', height: 7.1 },
+    { name: 'Ghoshal', height: 6 }, 
+    { name: 'Zeeshan', height: 5.8 }
+]
+const selectedPlayers = teamPlayers.filter(applyer => {
+    return applyer.height >= 5.8
+})
+selectedPlayers.forEach(e => console.log(e.name));
+
+
+
+// Map Method
+const products = [
+    {name: 'gold star', price: 20},
+    {name: 'mushroom', price: 40}, 
+    {name: 'green shell', price: 30}, 
+    {name: 'banana skin', price: 10}, 
+    {name: 'red shells', price: 50}
+]
+
+const sale = products.map(product => {
+    if(product.price > 30) {
+        // product.price = product.price / 2; <<<<<<<<  I shouldn't do that because it will change the prices in the original array
+        // return product;
+        return {name: product.name, price: product.price / 2 }
+    }else {
+        return product
+    }
+});
+console.log(sale);
+console.log(products);
 
 
 
